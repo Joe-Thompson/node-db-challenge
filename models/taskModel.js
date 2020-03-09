3a6e7fa1e26c8aa522506939c98ee89667cb5e38
Joe <joe@pop-os.localdomain>
2020-03-08 21:55:46 -0400

    fixied tasks by project id

--- models/taskModel.js
+++ models/taskModel.js
@@ -15,12 +15,20 @@ async function insertTask(tas) {
 }
 
 function getById(id) {
-    return db('task as t')
-        .join('project as p', 'p.id', 't.project_id')
-        .where('p.id', id)
-        .select('p.name as project',
-            'p.description',
-            't.description as task',
-            't.note')
-        .orderBy('t.id')
+    // return  db('task as t')
+    //     .join('project as p', 'p.id', 't.project_id')
+    //     .where({'p.id': id})
+    //     .select('p.name as project',
+    //         'p.description',
+    //         't.description as task',
+    //         't.note')
+    //     .orderBy('t.id');
+
+    const projectQ = db('project').where({ id }).first();
+    const tasksQ = db('task').where('project_id', id);
+    return Promise.all([projectQ, tasksQ])
+        .then(([project, task]) => {
+            project.task = task;
+            return project
+        })
 }
